69065a84454c7b84182da67ef4a79ee6f13d7748
Tom Shaw <Tomshaw650@gmail.com>
2023-06-06 12:11:11 +0100

    init

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -14,7 +14,9 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
+        <svg width="24" height="24">
+          <image xlinkHref="devil.svg" width="24" height="24" />
+        </svg>
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Tradepost - Diablo IV Trading",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "A site for listing and finding rares in Diablo IV.",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +11,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/tomshawdev",
+    github: "https://github.com/tomshaw650/tradepost",
   },
 }
