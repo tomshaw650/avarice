5286fc30446e699c251517d5088535d4f776d419
Tom Shaw <Tomshaw650@gmail.com>
2023-06-06 14:09:49 +0100

    environment variable hell.

--- lib/env.ts
+++ lib/env.ts
@@ -7,11 +7,13 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     DATABASE_URL: z.string().min(1),
     NEXTAUTH_URL: z.string().url().min(1),
+    NEXTAUTH_SECRET: z.string().min(1),
   },
   runtimeEnv: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
 });
