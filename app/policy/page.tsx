e0229bd074d86c8fe01f131c57d223fc2a278b96
Tom Shaw <Tomshaw650@gmail.com>
2023-06-06 13:54:37 +0100

    add policy

---
+++ app/policy/page.tsx
@@ -0,0 +1,16 @@
+export default function Policy() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <p>
+          Personal Data processed for the following purposes and using the
+          following services: Registration and authentication Google OAuth<br />
+          Personal Data: various types of Data as specified in the privacy
+          policy of the service<br />
+          Contact information Owner and Data Controller
+          Tradepost Owner contact email: tomshaw650@gmail.com
+        </p>
+      </div>
+    </section>
+  )
+}
