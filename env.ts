ab0dfee567e217b291aaab7cf009c711a1a61001
Tom Shaw <Tomshaw650@gmail.com>
2023-06-06 14:00:08 +0100

    update env

--- env.ts
+++ env.ts
@@ -5,11 +5,11 @@ export const env = createEnv({
   server: {
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
-    NODE_ENV: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
   },
   runtimeEnv: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 });
