39f6246af9f2bc65797cd6340bca897e93821a03
Tom Shaw <Tomshaw650@gmail.com>
2023-06-06 12:13:10 +0100

    remove providers for deployment

--- app/auth/[...nextauth]/route.ts
+++ app/auth/[...nextauth]/route.ts
@@ -3,11 +3,6 @@ import BattleNetProvider from "next-auth/providers/battlenet";
 
 const handler = NextAuth({
   providers: [
-    BattleNetProvider({
-      clientId: process.env.BATTLENET_CLIENT_ID,
-      clientSecret: process.env.BATTLENET_CLIENT_SECRET,
-      issuer: process.env.BATTLENET_ISSUER
-    })
   ]
 })
 
